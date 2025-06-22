1b5ba7e1dda39f5b03e5e6a0b3318be459b5cac3
Yinyug <ivy55786224@gmail.com>
2025-06-23 00:11:45 +0800

    Delete Lab10/Lab10 directory

--- Lab10/Lab10/naughty.js
+++
@@ -1,3 +0,0 @@
-alert("Haha! Malicious script is loaded!");
-document.getElementsByTagName("h1")[0].innerHTML = "Hello? Modified by malicious script! <p style=\"color:blue; font-size:100px\">You want more huh???</p> Well";
-
